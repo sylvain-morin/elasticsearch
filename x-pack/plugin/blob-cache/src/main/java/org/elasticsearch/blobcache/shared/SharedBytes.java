d6c023c50762f43237795c04796b24cdb8b5ea2b
Nick Tindall <nick.tindall@elastic.co>
2024-08-12 12:56:23 +1000

    Update elasticsearch changes

--- x-pack/plugin/blob-cache/src/main/java/org/elasticsearch/blobcache/shared/SharedBytes.java
+++ x-pack/plugin/blob-cache/src/main/java/org/elasticsearch/blobcache/shared/SharedBytes.java
@@ -172,6 +172,7 @@ public class SharedBytes extends AbstractRefCounted {
      * @param buf bytebuffer to use for writing
      * @throws IOException on failure
      */
+    // TODO: Add CacheCopyMetricsConsumer when required
     public static void copyToCacheFileAligned(
         IO fc,
         InputStream input,
@@ -204,6 +205,25 @@ public class SharedBytes extends AbstractRefCounted {
         }
     }
 
+    public interface CacheCopyMetricsConsumer {
+
+        CacheCopyMetricsConsumer NOOP = (br, rtn, bw, wtn, et) -> {};
+
+        /**
+         * Notify the times taken to read from the input stream and write to the destination
+         *
+         * Note that <code>bytesRead</code> will often differ from <code>bytesWritten</code> because we write
+         * in {@link #PAGE_SIZE} chunks
+         *
+         * @param bytesRead The number of bytes read
+         * @param readTimeNanos The time in nanoseconds taken to read those bytes
+         * @param bytesWritten The number of bytes written
+         * @param writeTimeNanos The time in nanoseconds taken to write those bytes
+         * @param elapsedTimeNanos The time in nanoseconds taken to copy the entire stream
+         */
+        void handleCopyMetrics(int bytesRead, long readTimeNanos, int bytesWritten, long writeTimeNanos, long elapsedTimeNanos);
+    }
+
     /**
      * Copy all bytes from {@code input} to {@code fc}, only doing writes aligned along {@link #PAGE_SIZE}.
      *
@@ -212,20 +232,37 @@ public class SharedBytes extends AbstractRefCounted {
      * @param fileChannelPos position in {@code fc} to write to
      * @param progressUpdater callback to invoke with the number of copied bytes as they are copied
      * @param buffer bytebuffer to use for writing
+     * @param metricsConsumer a consumer that will be provided with the read/write metrics upon completion of the copy
      * @return the number of bytes copied
      * @throws IOException on failure
      */
-    public static int copyToCacheFileAligned(IO fc, InputStream input, int fileChannelPos, IntConsumer progressUpdater, ByteBuffer buffer)
-        throws IOException {
+    public static int copyToCacheFileAligned(
+        IO fc,
+        InputStream input,
+        int fileChannelPos,
+        IntConsumer progressUpdater,
+        ByteBuffer buffer,
+        CacheCopyMetricsConsumer metricsConsumer
+    ) throws IOException {
+        final long copyStartTime = System.nanoTime();
         int bytesCopied = 0;
+        int totalBytesRead = 0;
+        long totalReadTimeNanos = 0L;
+        long totalWriteTimeNanos = 0L;
         while (true) {
+            long readStartTimeNanos = System.nanoTime();
             final int bytesRead = Streams.read(input, buffer, buffer.remaining());
+            totalReadTimeNanos += System.nanoTime() - readStartTimeNanos;
             if (bytesRead <= 0) {
                 break;
             }
+            long writeStartTimeNanos = System.nanoTime();
             bytesCopied += copyBufferToCacheFileAligned(fc, fileChannelPos + bytesCopied, buffer);
+            totalWriteTimeNanos += System.nanoTime() - writeStartTimeNanos;
             progressUpdater.accept(bytesCopied);
         }
+        long elapsedTimeNanos = System.nanoTime() - copyStartTime;
+        metricsConsumer.handleCopyMetrics(totalBytesRead, totalReadTimeNanos, bytesCopied, totalWriteTimeNanos, elapsedTimeNanos);
         return bytesCopied;
     }
 
