7ce7591e24f00dc9107c56953f6f2cfb72de43a4
Luke Whiting <luke.whiting@elastic.co>
2024-11-11 14:27:17 +0000

    SNAPSHOT More readable tests

--- x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/scheduler/CronTimezoneTests.java
+++ x-pack/plugin/core/src/test/java/org/elasticsearch/xpack/core/scheduler/CronTimezoneTests.java
@@ -16,6 +16,7 @@ import java.time.ZonedDateTime;
 import java.time.zone.ZoneOffsetTransition;
 import java.time.zone.ZoneRules;
 
+import static java.time.Instant.ofEpochMilli;
 import static java.util.TimeZone.getTimeZone;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.not;
@@ -104,6 +105,137 @@ public class CronTimezoneTests extends ESTestCase {
 
         assertThat(nextUtcScheduleBefore - utcEpochBefore, not(equalTo(nextUtcScheduleAfter - utcEpochAfter)));
 
+    }
+
+    private ZoneId generateRandomDSTZone() {
+        ZoneId timeZone;
+        int i = 0;
+        boolean found;
+        do {
+            timeZone = randomZone();
+            found = getTimeZone(timeZone).useDaylightTime();
+            i++;
+        } while (found == false && i <= 500); // Infinite loop prevention
+
+        if (found == false) {
+            fail("Could not find a timezone with DST");
+        }
+
+        logger.debug("Testing for timezone {} after {} iterations", timeZone, i);
+        return timeZone;
+    }
+
+    public void testForGMTAdvanceTransitionTriggerTimeIsRoundedToAfterDiscontinuity() {
+        ZoneId london = ZoneId.of("Europe/London");
+        Cron cron = new Cron("0 30 1 * * ?", getTimeZone(london)); // Every day at 1:30
+
+        Instant beforeTransition = Instant.parse("2025-03-30T00:00:00Z");
+        long beforeTransitionEpoch = beforeTransition.toEpochMilli();
+
+        long nextValidTimeAfter = cron.getNextValidTimeAfter(beforeTransitionEpoch);
+        System.out.println("nextValidTimeAfter = " + nextValidTimeAfter);
+        assertThat(ofEpochMilli(nextValidTimeAfter), equalTo(Instant.parse("2025-03-30T01:30:00Z")));
+    }
+
+    public void testForGMTRetardTransitionTriggerSkipSecondExecution() {
+        ZoneId london = ZoneId.of("Europe/London");
+        Cron cron = new Cron("0 30 1 * * ?", getTimeZone(london)); // Every day at 01:30
+
+        Instant beforeTransition = Instant.parse("2024-10-27T00:00:00Z");
+        long beforeTransitionEpoch = beforeTransition.toEpochMilli();
+
+        long firstValidTimeAfter = cron.getNextValidTimeAfter(beforeTransitionEpoch);
+        System.out.println("nextValidTimeAfter = " + firstValidTimeAfter);
+        assertThat(ofEpochMilli(firstValidTimeAfter), equalTo(Instant.parse("2024-10-27T00:30:00Z")));
+
+        long nextValidTimeAfter = cron.getNextValidTimeAfter(firstValidTimeAfter);
+        System.out.println("nextValidTimeAfter = " + nextValidTimeAfter);
+        assertThat(ofEpochMilli(nextValidTimeAfter), equalTo(Instant.parse("2024-10-28T01:30:00Z")));
+    }
+
+    // This test checks that once per minute crons will be unaffected by a DST transition
+    public void testDiscontinuityResolutionForNonHourCronInRandomTimezone() {
+        var timezone = generateRandomDSTZone();
+
+        var cron = new Cron("0 * * * * ?", getTimeZone(timezone)); // Once per minute
+
+        Instant referenceTime = randomInstantBetween(Instant.now(), Instant.now().plus(1826, ChronoUnit.DAYS)); // ~5 years
+        ZoneOffsetTransition transition1 = timezone.getRules().nextTransition(referenceTime);
+
+        // Currently there are no known timezones with DST transitions shorter than 10 minutes but this guards against future changes
+        if (Math.abs(transition1.getOffsetBefore().getTotalSeconds() - transition1.getOffsetAfter().getTotalSeconds()) < 600) {
+            fail("Transition is not long enough to test");
+        }
+
+        testNonHourCronTransition(transition1, cron);
+
+        var transition2 = timezone.getRules().nextTransition(transition1.getInstant().plus(1, ChronoUnit.DAYS));
+
+        testNonHourCronTransition(transition2, cron);
+
+    }
+
+    private static void testNonHourCronTransition(ZoneOffsetTransition transition, Cron cron) {
+        Instant insideTransition;
+        if (transition.isGap()) {
+            insideTransition = transition.getInstant().plus(10, ChronoUnit.MINUTES);
+            Instant nextTrigger = ofEpochMilli(cron.getNextValidTimeAfter(insideTransition.toEpochMilli()));
+            assertThat(nextTrigger, equalTo(insideTransition.plus(1, ChronoUnit.MINUTES)));
+        } else {
+            insideTransition = transition.getInstant().minus(10, ChronoUnit.MINUTES);
+            Instant nextTrigger = ofEpochMilli(cron.getNextValidTimeAfter(insideTransition.toEpochMilli()));
+            assertThat(nextTrigger, equalTo(insideTransition.plus(1, ChronoUnit.MINUTES)));
+
+            insideTransition = insideTransition.plus(transition.getDuration());
+            nextTrigger = ofEpochMilli(cron.getNextValidTimeAfter(insideTransition.toEpochMilli()));
+            assertThat(nextTrigger, equalTo(insideTransition.plus(1, ChronoUnit.MINUTES)));
+        }
+    }
+
+    // This test checks that once per minute crons will be unaffected by a DST transition
+    public void testDiscontinuityResolutionForCronInRandomTimezone() {
+        var timezone = generateRandomDSTZone();
+        timezone = ZoneId.of("Europe/London");
+
+        Instant referenceTime = randomInstantBetween(Instant.now(), Instant.now().plus(1826, ChronoUnit.DAYS)); // ~5 years
+        ZoneOffsetTransition transition1 = timezone.getRules().nextTransition(referenceTime);
+
+        // Currently there are no known timezones with DST transitions shorter than 10 minutes but this guards against future changes
+        if (Math.abs(transition1.getOffsetBefore().getTotalSeconds() - transition1.getOffsetAfter().getTotalSeconds()) < 600) {
+            fail("Transition is not long enough to test");
+        }
+
+        testHourCronTransition(transition1, timezone);
+
+        var transition2 = timezone.getRules().nextTransition(transition1.getInstant().plus(1, ChronoUnit.DAYS));
+
+        testHourCronTransition(transition2, timezone);
+    }
+
+    private static void testHourCronTransition(ZoneOffsetTransition transition, ZoneId timezone) {
+        if (transition.isGap()) {
+            LocalDateTime targetTime = transition.getDateTimeBefore().plusMinutes(10);
+
+            var cron = new Cron("0 " + targetTime.getMinute() + " " + targetTime.getHour() + " * * ?", getTimeZone(timezone));
+
+            long nextTrigger = cron.getNextValidTimeAfter(transition.getInstant().minus(10, ChronoUnit.MINUTES).toEpochMilli());
+
+            assertThat(ofEpochMilli(nextTrigger), equalTo(transition.getInstant().plusSeconds(600)));
+        } else {
+            LocalDateTime targetTime = transition.getDateTimeAfter().plusMinutes(10);
+            var cron = new Cron("0 " + targetTime.getMinute() + " " + targetTime.getHour() + " * * ?", getTimeZone(timezone));
+
+            long firstTrigger = cron.
+                getNextValidTimeAfter(
+                transition.getInstant().minusSeconds(transition.getDuration().toSeconds()).minus(10, ChronoUnit.MINUTES).toEpochMilli()
+            );
+
+            assertThat(ofEpochMilli(firstTrigger), equalTo(transition.getInstant().plusSeconds(600)));
+
+            var repeatTrigger = firstTrigger + (1000 * 10L);
+
+            assertThat(repeatTrigger - firstTrigger, Matchers.greaterThan(1000 * 60 * 60 * 6L));
+        }
     }
 
 }
